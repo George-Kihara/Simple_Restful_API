5cfde2623258bbb5623c572750b5489b38a832ae
GeorgeKihara <gegohcomp@gmail.com>
2017-07-05 08:30:01 +0300

    api

---
+++ routes/tasks.js
@@ -0,0 +1,81 @@
+var express = require('express');
+var router = express.Router();
+var mongojs = require('mongojs');
+var db = mongojs('mongodb://georgie_gegoh:georgiegegoh89@ds149382.mlab.com:49382/eazytoi',['tasks']);
+
+//Get all tasks
+router.get('/tasks', function(req, res, next){
+    db.tasks.find(function(err,tasks){
+        if(err){
+            res.send(err);
+        }
+        res.json(tasks);
+    });
+});
+
+//Get single task
+router.get('/tasks/:id', function(req, res, next){
+    db.tasks.findOne({_id: mongojs.ObjectId(req.params.id)}, function(err,task){
+        if(err){
+            res.send(err);
+        }
+        res.json(task);
+    });
+});
+
+//Save task
+router.post('/task', function(req,res,next){
+    var task = req.body;
+     if (!task.title || (task.isDone + '')){
+         res.status(400);
+         res.json({
+             "error": "Data is not valid"
+         });
+     }else {
+         db.tasks.save(task,function(err, task){
+            if (err){
+                res.send(err);
+            }
+            res.json(task);
+         });
+     }
+});
+
+//Delete a task
+router.delete('task/:id', function(req,res,next){
+    db.task.remove({_id:mongojs.ObjectId(req.params.id)}, function(err, task){
+        if(err){
+            res.send(err);
+        }
+        res.json(task);
+    });
+});
+
+//update task
+router.put('/task/:id', function(req,res,next){
+    var task = req.body;
+    var upTask = {};
+
+    if(task.isDone){
+        upTask.isDone = task.isDone;
+    }
+    if(task.title){
+        upTask.title = task.title;
+    }
+
+    if (!upTask){
+        res.status(400);
+        res.json({
+            "error" : "Bad Data"
+        });
+    }else {
+        db.tasks.update({_id: mongojs.ObjectId(req.params.id)}, upTask, {}, function(err,task){
+            if(err){
+                res.send(err);
+            }
+            res.json(task);
+        });
+    }
+});
+
+module.exports = router;
